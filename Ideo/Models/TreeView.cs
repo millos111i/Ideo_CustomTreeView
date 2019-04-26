8a26f763d6db12afa7d09f7974a99b7b51c408be
Kamil Wlazło <kamil119988@wp.pl>
2019-04-27 00:11:27 +0200

    Validation added

--- Ideo/Models/TreeView.cs
+++ Ideo/Models/TreeView.cs
@@ -8,6 +8,7 @@ namespace Ideo.Models
         public int Id { get; set; }
 
         [Required]
+        [RegularExpression(@"^[A-Za-z\s-]+$",ErrorMessage ="String may contain letters and spaces or dash")]
         public string Text { get; set; }
         [Display(Name ="Parent")]
         public int? ParentID { get; set; }
